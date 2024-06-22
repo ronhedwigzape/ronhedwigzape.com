61b6e98887f978ef2b8d976e32067e366f7c1b76
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-06-22 18:23:29 +0800

    refactor: remove authentication features in jetstream

--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,16 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+
     ]);
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+Route::middleware(['web'])->group(function () {
+    // Include all Fortify routes
+    require base_path('vendor/laravel/fortify/routes/routes.php');
+
+    // Override the /login route to return a 404 error
+    Route::match(['get', 'post'], '/login', function () {
+        abort(404);
+    });
 });
